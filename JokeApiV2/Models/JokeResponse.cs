315ef894d47f59704ef8b30f5f88e6b07b2c9bf7
Karan Nanda <ksnfury@gmail.com>
2023-06-28 15:06:06 +0530

    Adding JokeResponse and JokeSearchResponse Pojos for single and list of jokes respectively

---
+++ JokeApiV2/Models/JokeResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace JokeApiV2.Models
+{
+    public class JokeResponse
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("joke")]
+        public string Joke { get; set; }
+    }
+
+}
+
---
+++ JokeApiV2/Models/JokeSearchResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace JokeApiV2.Models
+{
+    public class JokeSearchResponse
+    {
+        [JsonProperty("results")]
+        public List<JokeResponse> Results { get; set; }
+
+        [JsonProperty("status")]
+        public int Status { get; set; }
+    }
+}
+
